b38ba06280f1f21b222be772b58090a8fbe91a9f
Patrick Tone <patrick@madronesoftware.com>
2020-12-03 13:01:10 +0100

    read input from file

--- 2020/fsharp/test/Day01.Tests/Day01.Tests.fs
+++ 2020/fsharp/test/Day01.Tests/Day01.Tests.fs
@@ -3,6 +3,12 @@ module Day01.Tests
 open FsUnit.Xunit
 open Xunit
 
+let private input path =
+    path
+    |> System.IO.File.ReadLines
+    |> Seq.map int
+    |> Seq.toList
+
 
 [<Fact>]
 let ``Should find the product of the two entries whose sum equals 2020`` () =
@@ -20,7 +26,8 @@ let ``Should find the product of the two entries whose sum equals 2020`` () =
 let ``Should find the product of the two entries for day 1`` () =
     let expected: Result<int, string> = Ok 605364
 
-    Input.entries ()
+    "input.txt"
+    |> input
     |> expenseReport
     |> should equal expected
 
@@ -40,6 +47,7 @@ let ``Should find the product of the three entries whose sum equals 2020`` () =
 let ``Should find the product of the three entries for day 1`` () =
     let expected: Result<int, string> = Ok 128397680
 
-    Input.entries ()
+    "input.txt"
+    |> input
     |> expenseReport2
     |> should equal expected
