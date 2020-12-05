ed72bd63887ee53077e5b928a35f4f91dc821665
Patrick Tone <patrick@madronesoftware.com>
2020-12-05 07:17:56 +0100

    completed day 3

--- 2020/fsharp/src/Day03/Library.fs
+++ 2020/fsharp/src/Day03/Library.fs
@@ -1,5 +1,14 @@
-﻿namespace Day03
+﻿module Day03
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+let encounterTrees (slopeX, slopeY) (slope: string seq) =
+    let hasTree (slopeX, slopeY) (index: int, landscape: string): bool =
+        let down = (index / slopeY)
+        let right = (down * slopeX) % landscape.Length
+
+        index % slopeY = 0 && landscape.[right] = '#'
+
+    slope
+    |> Seq.indexed
+    |> Seq.skip (1)
+    |> Seq.filter (hasTree (slopeX, slopeY))
+    |> Seq.length
--- 2020/fsharp/test/Day03.Tests/Day03.Tests.fs
+++ 2020/fsharp/test/Day03.Tests/Day03.Tests.fs
@@ -3,8 +3,74 @@ module Day03.Tests
 open FsUnit.Xunit
 open Xunit
 
-[<Fact>]
-let ``My test`` () =
-    let expected = true
+open Day03
 
-    true |> should equal expected
+let private input path = path |> System.IO.File.ReadLines
+
+[<Fact>]
+let ``Should encounter 7 trees on the given slope while going right 3 and down 1`` () =
+    let expected = 7
+
+    encounterTrees
+        (3, 1)
+        [ "..##......."
+          "#...#...#.."
+          ".#....#..#."
+          "..#.#...#.#"
+          ".#...##..#."
+          "..#.##....."
+          ".#.#.#....#"
+          ".#........#"
+          "#.##...#..."
+          "#...##....#"
+          ".#..#...#.#" ]
+    |> should equal expected
+
+[<Fact>]
+let ``Should encounter 282 trees on the given slope while going right 3 and down 1`` () =
+    let expected = 282
+
+    "input.txt"
+    |> input
+    |> (encounterTrees (3, 1))
+    |> should equal expected
+
+
+[<Fact>]
+let ``Should encounter 336 trees on the given slope while traversing at different angles`` () =
+    let expected = 336
+
+    [ (1, 1)
+      (3, 1)
+      (5, 1)
+      (7, 1)
+      (1, 2) ]
+    |> Seq.map (fun angle ->
+        encounterTrees
+            angle
+            [ "..##......."
+              "#...#...#.."
+              ".#....#..#."
+              "..#.#...#.#"
+              ".#...##..#."
+              "..#.##....."
+              ".#.#.#....#"
+              ".#........#"
+              "#.##...#..."
+              "#...##....#"
+              ".#..#...#.#" ])
+    |> Seq.fold (*) 1
+    |> should equal expected
+
+[<Fact>]
+let ``Should encounter 958815792 trees on the given slope while traversing at different angles`` () =
+    let expected = 958815792
+
+    [ (1, 1)
+      (3, 1)
+      (5, 1)
+      (7, 1)
+      (1, 2) ]
+    |> Seq.map (fun angle -> encounterTrees angle (input "input.txt"))
+    |> Seq.fold (*) 1
+    |> should equal expected
